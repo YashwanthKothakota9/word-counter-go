2d5fa0a5db75d3991167abe4f06e67208f142626
YashwanthK <yashwanthkothakota@gmail.com>
2025-12-14 12:11:16 +0530

    add: two edge cases tests

--- main_test.go
+++ main_test.go
@@ -6,9 +6,30 @@ func TestCountWords(t *testing.T) {
 	input := "one two three four five"
 	wants := 5
 
-	result := countWords([]byte(input))
+	result := CountWords([]byte(input))
 
 	if result != wants {
+		t.Log("expected:", wants, "got:", result)
+		t.Fail()
+	}
+
+	input = ""
+	wants = 0
+
+	result = CountWords([]byte(input))
+
+	if result != wants {
+		t.Log("expected:", wants, "got:", result)
+		t.Fail()
+	}
+
+	input = " "
+	wants = 0
+
+	result = CountWords([]byte(input))
+
+	if result != wants {
+		t.Log("expected:", wants, "got:", result)
 		t.Fail()
 	}
 }
